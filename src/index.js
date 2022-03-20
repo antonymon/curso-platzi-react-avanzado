72a52a57deabf9bbc5018658dcaee32f96b83e5f
antonymon <monterrosoantony@gmail.com>
2022-03-20 00:08:56 -0600

    Feat: Intallation of  React and Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Start :D!');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(<h1>Hola Mundo!</h1>, document.getElementById('app'));
+
+// console.log('Start :D!');
